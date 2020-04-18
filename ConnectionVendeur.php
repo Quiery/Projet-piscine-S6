ea24b224c80b70cfff4f8a7d3e7f9d2318831b1c
clement-mace-montegut <clement.mace-montegut@edu.ece.fr>
2020-04-18 11:25:51 +0200

    modification

--- ConnectionVendeur.php
+++ ConnectionVendeur.php
@@ -5,194 +5,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" type="text/css" href="Piscine.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  
-
-  <style type="text/css">
-
-a {
-    color: #ffe841;
-    text-decoration: none;
-}
-
-
-  
- .btn-default {
-    width: 150px;
-    float: left;
-    margin: 0px 170px;
-}
-
-body{
-
-background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR7EJVYpfaWmsL3tYPzKrDF-z5sLZeR8FJzXVWyQH1KRnIB9krK&usqp=CAU);
-color: blanchedalmond;
-
-}
-
-.card-body{
-
-color: white;
-}
-
-#card-body-1 {
-    width: 68%;
-    margin: 0px 97px;
-    background-color: whitesmoke;
-    color: black;
-    padding: 5px 5px 0px;
-}
-
-
-@media (max-width: 600px) {
-    .carousel-caption {
-      display: none; 
-    }
-  }
-
-  @media screen and (min-width: 768px){
-  .carousel-control .glyphicon-chevron-left, .carousel-control .glyphicon-chevron-right, .carousel-control .icon-next, .carousel-control .icon-prev {
-    width: 30px;
-    height: 30px;
-    margin-top: -64px;
-    font-size: 60px;
-} }
-
-@media screen and (min-width: 768px){
-  .carousel-control .glyphicon-chevron-left, .carousel-control .icon-prev {
-    margin-left: -38px;
-}
-}
-@media screen and (min-width: 768px){
-.carousel-indicators {
-    bottom: -22px;
-}}
-
-
-.carousel-inner img {
-      width: 100%; /* Set width to 100% */
-      margin: auto;
-      min-height:200px;
-  }
-
-#col-carou{
-
-text-align: center;
-
-}
-
-#connexion{
-
-width: 300px;
-height: auto;
-padding: 5px 5px;
-border-radius: 6px;
-margin-bottom: 15px;
-
-}
-
-
-.container {
-    padding-right: 15px;
-    padding-left: 15px;
-    margin-right: auto;
-    margin-left: auto;
-    margin-bottom: 20px;
-}
-
-.dropdown, .dropup {
-    position: relative;
-    float: left;
-}
-
-.form-control{
-  border-radius: 0%;
-}
-.form-inline .has-feedback .form-control-feedback {
-    top: 8px;
-}
-
-#form_connect{
-    text-align: center;
-    color: black;
-    background-color: blanchedalmond;
-    margin-block-end: 20px;
-    margin-inline-start: 700px;
-    margin-inline-end: 700px;
-    padding-block-end: 21px;
-}
-
-#form_rechercher{
-
-border-left: 2px solid black;
-
-}
-
-#img_carou{
-
-width: auto;
-height: 230px;
-
-}
-
-.has-success .form-control-feedback {
-    color: #337ab7;
-}
-  .navbar {
-    position: relative;
-    min-height: 50px;
-    margin-bottom: 20px;
-    border: 2px solid black;
-    background-color: #9a4000;
-}
-
-.navbar-nav>li>.dropdown-menu {
-    margin-top: 2px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-    padding-left: 5px;
-    background-color: #9a4000;
-}
-.navbar-brand {
-    float: left;
-    height: 50px;
-    padding: 0px 15px;
-    font-size: 18px;
-    line-height: 20px;
-    border-right: 2px solid black;
-}
-  #rechercher{
-    margin-top: 0px;
-    margin-left: 0px;
-    width: 534px;
-    height: 50px;
-    border :0px;
-    padding-left: 25px;
-    border-right: 2px solid black;
-
-  }
-
-.navbar-right {
-    float: right!important;
-    margin-right: -15px;
-    border-left: 2px solid black;
-}
-.page-footer {
-    background-color: #222;
-    color: #ccc;
-    padding: 60px 0 30px;
-}
-.footer-copyright {
-    color: #666;
-    padding: 40px 0;
-}
-
-
-</style>
-
-
+ 
 </head>
 <body>
 
@@ -239,10 +55,10 @@ height: 230px;
 </nav>
 
 <form id="form_connect">
-    <h1>Connectez-vous pour acheter</h1><br>
+    <h1>Connectez-vous pour vendre</h1><br>
     <input id="connexion" type="text" placeholder="Login ou Email"><br>
     <input id="connexion" type="password" placeholder="Mot de Passe"><br>
-    <input type="submit" name="Valider" value="Valider"></input><br><br><br>
+    <input type="submit" name="Valider"></submit><br><br><br>
     <button>Creer un compte</button>
 </form>
 
