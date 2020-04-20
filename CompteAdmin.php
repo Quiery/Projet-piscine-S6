f4252861b40b37b2e5868a731b53e088a7983e1c
clement-mace-montegut <49635665+clement-mace-montegut@users.noreply.github.com>
2020-04-20 23:25:47 +0200

    modification compteAdmin

--- CompteAdmin.php
+++ CompteAdmin.php
@@ -1,66 +1,71 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Bootstrap Example</title>
+  <title>Compte Admin</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="Piscine.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
 </head>
 <body>
 
 <nav class="navbar navbar-expand-md">
-    <a class="navbar-brand" href="#"><img src="NGA.png" class="img-responsive" style="width: 70px; height: 50px;"></a>
+    <a class="navbar-brand" href="HomePage.php"><img src="https://le-marche-eclectique.com/wp-content/uploads/2017/03/tribunal-maillet.png" 
+    class="img-responsive" style="width: 70px; height: 50px;"></a>
     <div class="collapse navbar-collapse" id="main-navigation">
       <ul class="nav navbar-nav">
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Catégories <span class="caret"></span>
           </a>
           
           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Ferraille ou Trésor</a><br>
-            <a class="dropdown-item" href="#">Bon pour le Musée</a><br>
-            <a class="dropdown-item" href="#">Accessoire VIP</a>
+            <a class="dropdown-item" href="categorie.php?categorie=Feraille ou Trèsor">Ferraille ou Trésor</a><br>
+            <a class="dropdown-item" href="categorie.php?categorie=Bon pour le Musé">Bon pour le Musée</a><br>
+            <a class="dropdown-item" href="categorie.php?categorie=Accessoire VIP">Accessoire VIP</a>
           </div>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Achats <span class="caret"></span>
           </a>
           
           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Enchères</a><br>
-            <a class="dropdown-item" href="#">Achats Immédiats</a><br>
-            <a class="dropdown-item" href="#">Meilleur Offre</a>
+            <a class="dropdown-item" href="achats.php?achats=Enchères">Enchères</a><br>
+            <a class="dropdown-item" href="achats.php?achats=Achats Immediats">Achats Immédiats</a><br>
+            <a class="dropdown-item" href="achats.php?achats=Meilleur Offre">Meilleur Offre</a>
           </div>
         </li>
     </ul>
-        <form id="form_rechercher" class="form-inline nav navbar-nav" role="form">
+        <form action="Recherche.php" method="GET" id="form_rechercher" class="form-inline nav navbar-nav" role="form">
             <div class="form-group has-success has-feedback">
               <label class="control-label" for="rechercher"></label>
               <span class="glyphicon glyphicon-search form-control-feedback"></span>
-              <input class="nav navbar-nav form-control" type="text" class="" id="rechercher"  placeholder="Recherche">            
+              <input class="nav navbar-nav form-control" type="text" name="recherche" id="rechercher"  placeholder="Recherche">            
             </div>
           </form>
         <ul class="nav navbar-nav navbar-right">
-            <li><a class="nav-link" href="#"><span class="glyphicon glyphicon-usd"></span> Vendre</a></li>
-            <li><a class="nav-link" href="#"><span class="glyphicon glyphicon-user"></span> Mon Compte</a></li>
-            <li><a class="nav-link" href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Panier</a></li>
+            <li><a class="nav-link" href="vendre.php"><span class="glyphicon glyphicon-usd"></span> Vendre</a></li>
+            <li><a class="nav-link" href="VotreCompte.php"><span class="glyphicon glyphicon-user"></span> Mon Compte</a></li>
+            <li><a class="nav-link" href="Panier.php"><span class="glyphicon glyphicon-shopping-cart"></span> Panier</a></li>
         </ul>
     </div>
 </nav>
+
+
  
 <div class="container" style="text-align: center;" id="CompteAdmin">
     <h1>Compte Administrateur</h1><br>
-    <button type="button" id="button_admin" href="#">Mes articles en ventes</button><br>
-    <button type="button" id="button_admin" href="#">Ajouter un vendeur</button><br>
-    <button type="button" id="button_admin" href="#">Supprimer un vendeur</button>
+    <button type="button" id="button_admin" href="Vendre.php">Mes articles en ventes</button><br>
+    <button type="button" id="button_admin" href="vendeur_sign-in.php">Ajouter un vendeur</button><br>
+    <button type="button" id="button_admin" href="supprimer_vendeur.php">Supprimer un vendeur</button><br>
+    <button type="button" id="button_admin" href="supprimer_article.php">Supprimer un objet</button>
+    <form name="deconnexion" method="POST">
+        <input type="submit" id="button_admin_2" name="valider" value="Deconnexion"/>
+    </form>
 </div>
-
 
 
 
@@ -70,28 +75,56 @@
             <div class="col-lg-8 col-md-8 col-sm-12">
                 <h6 class="text-uppercase font-weight-bold">Information additionnelle</h6>
                 <p>
-                Vous pouvez nous retrouver tous les jours au local NGA en dessous de la cour E1.
-                Vous pourrez profiter de nos PC et consoles NewGen qui vous ferront passez le temps lors de vos longues pauses quotidiennes. 
-                </p>
-                <p>
-                N'hesitez pas à vous inscrire au bureau pour la somme de 10€.
-                Cette inscription vous donnera accès au local, ainsi qu'à des remises lors des évnements. 
-                Mais surtout cette somme nous permettera d'acquérir plus d'équipements et de vous préparer des évènements de plus grandes qualités.
+                Bienvenue dans un mileu de l'exploration d'un monde tout nouveau. Vous pourrez y trouver tout ce que vous désirez de la feraille à de vrais Trésors. Notre site vous propose plusieurs méthodes d'achats pour vous convenir au mieux.
+                Vous avez aussi la possibilité d'y vendre vos vieux objets ou au ceux que vous n'utilisez plus.
                 </p>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-12">
                 <h6 class="text-uppercase font-weight-bold">Contact</h6>
                 <p>
                 37, quai de Grenelle, 75015 Paris, France <br>
-                nga@ece.fr <br>
-                <a href="https://www.facebook.com/groups/NGA404/ ">https://www.facebook.com/groups/NGA404/ </a><br>
+                webDynamique@ece.fr <br>
+                <a href="https://www.ece.fr/ecole-ingenieur/%22%3Ehttps://www.ece.fr/ecole-ingenieur/</a><br>
                 </p>
             </div>
         </div>
     </div>
-    <div class="footer-copyright text-center">&copy; 2019 Copyright | Droit d'auteur: webDynamique.ece.fr</div>
+    <div class="footer-copyright text-center">&copy; 2019 Copyright | Droit d'auteur: webDynamique@ece.fr</div>
 </footer>
 
+<?php
+
+if($_POST(["valider"])){
+  Deconnexion();
+}
+
+function getIp(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+          $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+          $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+      }
+      
+ function Deconnexion()
+{
+  $database = "ebayece";
+
+  $db_handle = mysqli_connect('localhost','root','');
+  $db_found = mysqli_select_db($db_handle, $database);
+  $ip=getIp();
+  if ($db_found) 
+  {
+    $sql="UPDATE connexion_courante SET AcheteurID='NULL' where ip=$ip";
+    mysqli_close($db_handle);
+
+    echo "windows.location.assign(HomePage.php)";
+  }
+}
+?>
 
 </body>
 </html>
