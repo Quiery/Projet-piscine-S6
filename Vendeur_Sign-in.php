a3efe76aa800db9c8f066077e2e39fd1d977df93
clement-mace-montegut <49635665+clement-mace-montegut@users.noreply.github.com>
2020-04-20 23:49:33 +0200

    Final

--- Vendeur_Sign-in.php
+++ Vendeur_Sign-in.php
@@ -8,14 +8,6 @@
   <link rel="stylesheet" type="text/css" href="Piscine.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    
-<style type="text/css">
-#flex_sign_in {
-    display: flex;
-    margin: 0% 20%;
-    background-color: blanchedalmond;
-}
-    </style>
 </head>
 <body>
 
